5ac4556ee975edd5c5e42c2296cd6fd203045113
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-03-03 20:56:37 -0800

    Use base SetCloseButton

--- Dynamic Island/Settings/SettingsWindow.xaml.cs
+++ Dynamic Island/Settings/SettingsWindow.xaml.cs
@@ -9,6 +9,8 @@ namespace Dynamic_Island.Settings
         {
             this.InitializeComponent();
             SetTitleBar(titleBarArea);
+
+            SetCloseButton(close);
             Closed += (s, e) => App.SettingsWindow = null;
         }
 
@@ -19,13 +21,6 @@ namespace Dynamic_Island.Settings
             Rect minRect = new(50 * scale, 11 * scale, 12 * scale, 12 * scale);
 
             return [closeRect.ToRectInt32(), maxRect.ToRectInt32(), minRect.ToRectInt32()];
-        }
-        private void Close_PointerPressed(object sender, PointerRoutedEventArgs e) => (sender as Microsoft.UI.Xaml.Shapes.Ellipse).Fill = Brushes.DarkIndianRed;
-        private void Close_PointerExited(object sender, PointerRoutedEventArgs e) => (sender as Microsoft.UI.Xaml.Shapes.Ellipse).Fill = Brushes.IndianRed;
-        private void Close_PointerReleased(object sender, PointerRoutedEventArgs e)
-        {
-            (sender as Microsoft.UI.Xaml.Shapes.Ellipse).Fill = Brushes.IndianRed;
-            Close();
         }
 
         private void ChangeTab(SelectorBar sender, SelectorBarSelectionChangedEventArgs e) => view.Navigate(sender.SelectedItem.Text switch
